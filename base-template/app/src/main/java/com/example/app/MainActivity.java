15b5abfc5a19f78a3f502cf81e7ba5332005a5c3
why459097630 <whywhy9264@gmail.com>
2025-08-06 15:39:12 +0800

    Add files via upload

---
+++ base-template/app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,17 @@
+
+package com.example.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // GPT_INSERT_onCreate
+    }
+
+    // GPT_INSERT_methods
+}
