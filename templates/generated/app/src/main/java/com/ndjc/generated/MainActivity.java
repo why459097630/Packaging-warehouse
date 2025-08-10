f20a65c03da3b496bebf52fa63976341d3c0ce95
why459097630 <whywhy9264@gmail.com>
2025-08-10 19:56:57 +0800

    update templates/generated/app/src/main/java/com/ndjc/generated/MainActivity.java

--- templates/generated/app/src/main/java/com/ndjc/generated/MainActivity.java
+++ templates/generated/app/src/main/java/com/ndjc/generated/MainActivity.java
@@ -2,13 +2,14 @@ package com.ndjc.generated;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView diceImage;
+    private TextView diceTextView;
     private Random random;
 
     @Override
@@ -16,10 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        diceImage = findViewById(R.id.dice_image);
+        diceTextView = findViewById(R.id.diceTextView);
         random = new Random();
 
-        diceImage.setOnClickListener(new View.OnClickListener() {
+        diceTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 rollDice();
@@ -28,32 +29,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void rollDice() {
-        int randomNumber = random.nextInt(6) + 1;
-        int drawableResource;
-
-        switch (randomNumber) {
-            case 1:
-                drawableResource = R.drawable.dice_1;
-                break;
-            case 2:
-                drawableResource = R.drawable.dice_2;
-                break;
-            case 3:
-                drawableResource = R.drawable.dice_3;
-                break;
-            case 4:
-                drawableResource = R.drawable.dice_4;
-                break;
-            case 5:
-                drawableResource = R.drawable.dice_5;
-                break;
-            case 6:
-                drawableResource = R.drawable.dice_6;
-                break;
-            default:
-                drawableResource = R.drawable.dice_1;
-        }
-
-        diceImage.setImageResource(drawableResource);
+        int diceNumber = random.nextInt(6) + 1;
+        diceTextView.setText(String.valueOf(diceNumber));
     }
 }
