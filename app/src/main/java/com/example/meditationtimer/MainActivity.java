e6f166e640d3c8c77ab3364dae71a296f864bf4e
why459097630 <whywhy9264@gmail.com>
2025-08-17 21:35:34 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/meditationtimer/MainActivity.java
+++ app/src/main/java/com/example/meditationtimer/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.meditationtimer;
+
 import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
   @Override protected void onCreate(Bundle s){
     super.onCreate(s);
