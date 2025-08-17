ceba92bd909a8fb822599a48540ef9f6f4cbe0e2
why459097630 <whywhy9264@gmail.com>
2025-08-17 22:43:16 +0800

    chore(gen): update MainActivity.java

--- app/src/main/java/com/example/meditationtimer/MainActivity.java
+++ app/src/main/java/com/example/meditationtimer/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.meditationtimer;
-
 import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
   @Override protected void onCreate(Bundle s){
     super.onCreate(s);
     setContentView(R.layout.activity_main);
     TextView tv = findViewById(R.id.textHello);
-    if (tv != null) tv.setText("Hello from API code");
+    if(tv!=null) tv.setText(getString(R.string.hello_text));
   }
 }
