8e5595297266d16b8028f7c3933d4c74343ad984
why459097630 <whywhy9264@gmail.com>
2025-08-26 15:48:12 +0800

    NDJC:64uiqfywmdxmes8urmf apply plan for "Dice Roller"

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -11,9 +11,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // NDJC:ONCREATE
+((android.widget.Button) findViewById(R.id.btnRoll)).setOnClickListener(v -> ((android.widget.TextView) findViewById(R.id.tvResult)).setText(String.valueOf(1 + new java.util.Random().nextInt(6))));
+
         // 生成器会在这里插入事件绑定（如为按钮设置 onClickListener）
     }
 
     // NDJC:FUNCTIONS
+// dice functions
+
     // 生成器需要时会在这里追加辅助方法
 }
