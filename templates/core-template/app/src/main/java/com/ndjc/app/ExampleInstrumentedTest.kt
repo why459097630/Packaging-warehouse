894401ca3efc98119444f20f5aeb7838e19a9a2a
why459097630 <whywhy9264@gmail.com>
2025-09-17 14:18:45 +0800

    Create ExampleInstrumentedTest.kt

---
+++ templates/core-template/app/src/main/java/com/ndjc/app/ExampleInstrumentedTest.kt
@@ -0,0 +1,11 @@
+package com.ndjc.app
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    // NDJC:ANDROID_TESTS_SETUP   // Hilt/ComposeTestRule/GrantPermissionRule 等
+    @Test fun launch() { /* NDJC:ANDROID_TESTS */ }
+}
