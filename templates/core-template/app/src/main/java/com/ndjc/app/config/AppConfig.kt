4d0f26e0c15417f1e703c76d3410e403965d3cb3
why459097630 <whywhy9264@gmail.com>
2025-09-18 10:53:50 +0800

    Create AppConfig.kt

---
+++ templates/core-template/app/src/main/java/com/ndjc/app/config/AppConfig.kt
@@ -0,0 +1,10 @@
+package com.ndjc.app.config
+
+object AppConfig {
+    // NDJC:API_KEY
+    // NDJC:IMAGE_BASE_URL
+    // NDJC:DYNAMIC_COLOR_ENABLED
+    // NDJC:JOBS_ON
+    // NDJC:BUILD_FLAVOUR
+    // NDJC:APP_THEME_PRESET
+}
