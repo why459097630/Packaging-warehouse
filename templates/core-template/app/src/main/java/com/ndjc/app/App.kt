a748dcb9bfa324067ed60f16382582c24e38d4ca
why459097630 <whywhy9264@gmail.com>
2025-09-18 10:26:49 +0800

    Update App.kt

--- templates/core-template/app/src/main/java/com/ndjc/app/App.kt
+++ templates/core-template/app/src/main/java/com/ndjc/app/App.kt
@@ -5,14 +5,24 @@ import android.app.Application
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+
         // 各类 SDK/初始化
         // NDJC:BLOCK:STARTUP
         // NDJC:BLOCK:STARTUP_INITIALIZERS
+
+        // 从 BuildConfig 读取的文本锚点（生成器会注入实际值）
+        val startupLibs       = BuildConfig.STARTUP_LIBS        // NDJC:STARTUP_LIBS
+        val startupInitializr = BuildConfig.STARTUP_INITIALIZERS // NDJC:STARTUP_INITIALIZERS
+        @Suppress("UNUSED_VARIABLE")
+        val _keepRefs = arrayOf(startupLibs, startupInitializr)  // 防被 R8 消掉
+
+        // 埋点 / 崩溃 / 远程配置 / 开关 / 实验
         // NDJC:BLOCK:ANALYTICS
         // NDJC:BLOCK:CRASH
         // NDJC:BLOCK:REMOTE_CONFIG
         // NDJC:BLOCK:FEATURE_FLAGS
         // NDJC:BLOCK:AB_TEST
+
         // 依赖注入容器
         // NDJC:BLOCK:DI_CONTAINER
     }
