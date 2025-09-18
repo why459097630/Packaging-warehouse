63105029b3c429d1cf35e230a6ed98afaa1cf01f
why459097630 <whywhy9264@gmail.com>
2025-09-18 12:33:56 +0800

    Update App.kt

--- templates/core-template/app/src/main/java/com/ndjc/app/App.kt
+++ templates/core-template/app/src/main/java/com/ndjc/app/App.kt
@@ -6,21 +6,22 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        // 各类 SDK/初始化
+        // 各类 SDK / 初始化
         // NDJC:ANALYTICS_SDK
         // NDJC:CRASH_SDK
         // NDJC:PUSH_SDK
 
+        // 框架化启动钩子
         // NDJC:BLOCK:STARTUP
         // NDJC:BLOCK:STARTUP_INITIALIZERS
 
         // 从 BuildConfig 读入（保持引用避免被 R8 清理）
-        val startupLibs        = BuildConfig.STARTUP_LIBS         // NDJC:STARTUP_LIBS
-        val startupInitializers= BuildConfig.STARTUP_INITIALIZERS // NDJC:STARTUP_INITIALIZERS
+        val startupLibs         = BuildConfig.STARTUP_LIBS          // NDJC:STARTUP_LIBS
+        val startupInitializers = BuildConfig.STARTUP_INITIALIZERS  // NDJC:STARTUP_INITIALIZERS
         @Suppress("UNUSED_VARIABLE")
         val _keepRefs = arrayOf(startupLibs, startupInitializers)
 
-        // 远程配置/开关/实验
+        // 远程配置 / 开关 / 实验
         // NDJC:BLOCK:REMOTE_CONFIG
         // NDJC:BLOCK:FEATURE_FLAGS
         // NDJC:BLOCK:AB_TEST
