99714f6a3ca762d87922b4db5c009b87df00562e
why459097630 <whywhy9264@gmail.com>
2025-09-19 21:32:30 +0800

    [NDJC 2025-09-19T13-31-30-393Z] sync app app/src/main/java/com/ndjc/app/integration/ServiceHooks.kt

---
+++ app/src/main/java/com/ndjc/app/integration/ServiceHooks.kt
@@ -0,0 +1,16 @@
+package com.ndjc.app.integration
+
+object ServiceHooks {
+    fun initOnAppStart(/* ctx: Context */) {
+
+    }
+
+    fun onUserLogin(/* uid: String */) {
+
+    }
+
+    fun onUserLogout() {
+
+    }
+
+}
