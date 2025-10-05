2cab3773654e23c3d1e57b3a3237fa694eba845a
why459097630 <whywhy9264@gmail.com>
2025-10-05 18:59:37 +0800

    Create SplashScreen.kt

---
+++ templates/circle-basic/app/src/main/java/com/ndjc/app/ui/screens/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.ndjc.app.ui.screens  // NDJC:PACKAGE_NAME
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+
+@Composable
+fun SplashScreen() {
+  // BLOCK:SPLASH_CONTENT
+  Surface { Box(Modifier.fillMaxSize()) { /* Splash placeholder */ } }
+  // END_BLOCK
+}
