5467ef215c90118776b7c294a5dae08346724b48
why459097630 <whywhy9264@gmail.com>
2025-10-05 22:20:44 +0800

    Update SplashScreen.kt

--- templates/circle-basic/app/src/main/java/com/ndjc/app/ui/screens/SplashScreen.kt
+++ templates/circle-basic/app/src/main/java/com/ndjc/app/ui/screens/SplashScreen.kt
@@ -1,8 +1,9 @@
 package com.ndjc.app.ui.screens  // NDJC:PACKAGE_NAME
 
-import androidx.compose.material3.*
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Modifier
 
 @Composable
 fun SplashScreen() {
