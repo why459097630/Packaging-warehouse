37716f7ed8b616237310b905c443d669c2602ff1
why459097630 <whywhy9264@gmail.com>
2025-10-06 22:19:30 +0800

    Update TopBar.kt

--- templates/circle-basic/app/src/main/java/com/ndjc/app/feature/common/TopBar.kt
+++ templates/circle-basic/app/src/main/java/com/ndjc/app/feature/common/TopBar.kt
@@ -1,12 +1,12 @@
 @file:OptIn(androidx.compose.material3.ExperimentalMaterial3Api::class)
 
-package com.ndjc.app.feature.common
+package com.ndjc.app.feature.common  // 固定源码包名
 
-import androidx.annotation.StringRes          // ✅ 补上
+import androidx.annotation.StringRes
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource // ✅ 补上
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun TopBar(
